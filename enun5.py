35a2c1d1a0d474d59328bb941cac3bdefd5407e5
Carlos Jesús Sánchez <carlosjsanchezortega@gmail.com>
2016-02-28 21:35:47 +0100

    Enunciado 5 finalizado

--- enun5.py
+++ enun5.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Muestra solamente los nombres de los directorios en los que 
+# 5.Muestra solamente los nombres de los directorios en los que 
 #	el tipo de atributo sea igual a "Zona Wifi,Comunicaciones"
+
+
+from lxml import etree
+
+doc = etree.parse("/home/charlie/Escritorio/GitHub/ejercicio_xml/zona-wifi.xml")
+
+raiz = doc.getroot()
+
+directorio = raiz.findall("directorio")
+
+for d in directorio:
+	if d.attrib.get("tipo") == "Zona Wifi,Comunicaciones":
+		print d.findtext("nombre")
