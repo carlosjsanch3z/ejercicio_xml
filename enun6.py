89b6bda030785a0efccd84a5320cc80b26af2059
Carlos Jesús Sánchez <carlosjsanchezortega@gmail.com>
2016-03-07 12:45:49 +0100

    corregido la etiqueta img

--- enun6.py
+++ enun6.py
@@ -13,26 +13,25 @@ directorio = raiz.findall("directorio")
 
 var = raw_input("Subcadena: ")
 
-lista = []
-
 for d in directorio:
 	categorias = d.findall("categorias")
 	for c in categorias:
 		categoria = c.findall("categoria")
-		for ca in categoria:
-			if ca.text.startswith(var):
-				padre = ca.getparent().getparent()
-				nombre = padre.findtext("nombre")
-				horario = padre.findtext("horario")
-				imagen = padre.findtext("foto")
+		with open ("wifis.html", "w") as archivo:
+			for ca in categoria:
+				if ca.text.startswith(var):
+					padre = ca.getparent().getparent()
+					nombre = padre.findtext("nombre")
+					horario = padre.findtext("horario")
+					imagen = padre.findtext("foto")
 			
-				with open ("wifis.html", "w") as archivo:
+				
 					nombre = nombre.encode('utf-8')
 					horario = horario.encode('utf-8')
 
 					archivo.write("<h1>"+nombre+"</h1>"+"\n")
 					archivo.write("<p>"+horario+"</p>"+"\n")
-					archivo.write("<img src="+str(imagen)+"\n")
+					archivo.write("<img src="+str(imagen)+"/>"+"\n")
 				
 
 
