0f853cd35b21f977901b9e75873baac98be55ebe
Carlos Jesús Sánchez <carlosjsanchezortega@gmail.com>
2016-03-07 12:39:41 +0100

    Solucionar la url de la imagen

---
+++ enun6.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 6. Un programa que pida una categoria y genere un fichero html que muestre : el nombre, el horario y una img de la zona wifi.
+
+from lxml import etree
+
+doc = etree.parse("zona-wifi.xml")
+
+raiz = doc.getroot()
+
+directorio = raiz.findall("directorio")
+
+var = raw_input("Subcadena: ")
+
+lista = []
+
+for d in directorio:
+	categorias = d.findall("categorias")
+	for c in categorias:
+		categoria = c.findall("categoria")
+		for ca in categoria:
+			if ca.text.startswith(var):
+				padre = ca.getparent().getparent()
+				nombre = padre.findtext("nombre")
+				horario = padre.findtext("horario")
+				imagen = padre.findtext("foto")
+			
+				with open ("wifis.html", "w") as archivo:
+					nombre = nombre.encode('utf-8')
+					horario = horario.encode('utf-8')
+
+					archivo.write("<h1>"+nombre+"</h1>"+"\n")
+					archivo.write("<p>"+horario+"</p>"+"\n")
+					archivo.write("<img src="+str(imagen)+"\n")
+				
+
+
