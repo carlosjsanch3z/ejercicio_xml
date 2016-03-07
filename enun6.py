a3e7f4d2d2db2eee7103a514d5bf3d388e247ab3
Carlos Jesús Sánchez <carlosjsanchezortega@gmail.com>
2016-03-07 14:02:21 +0100

    Finish

--- enun6.py
+++ enun6.py
@@ -12,26 +12,36 @@ raiz = doc.getroot()
 directorio = raiz.findall("directorio")
 
 var = raw_input("Subcadena: ")
-
+lista=[]
+dicc= {}
 for d in directorio:
 	categorias = d.findall("categorias")
 	for c in categorias:
 		categoria = c.findall("categoria")
-		with open ("wifis.html", "w") as archivo:
-			for ca in categoria:
-				if ca.text.startswith(var):
-					padre = ca.getparent().getparent()
-					nombre = padre.findtext("nombre")
-					horario = padre.findtext("horario")
-					imagen = padre.findtext("foto")
+		for ca in categoria:
+			if ca.text.startswith(var):
+				padre = ca.getparent().getparent()
+				nombre = padre.findtext("nombre")
+				horario = padre.findtext("horario")
+				imagen = padre.findtext("foto")
 			
 				
-					nombre = nombre.encode('utf-8')
-					horario = horario.encode('utf-8')
+				nombre = nombre.encode('utf-8')
+				horario = horario.encode('utf-8')
 
-					archivo.write("<h1>"+nombre+"</h1>"+"\n")
-					archivo.write("<p>"+horario+"</p>"+"\n")
-					archivo.write("<img src="+str(imagen)+"/>"+"\n")
-				
+				dicc["nombre"]=nombre
+				dicc["horario"]=horario
+				dicc["imagen"]=imagen
+				lista.append(dicc)
+				dicc = {}
+
+
+with open ("wifis.html", "w") as archivo:
+
+	for elemento in lista:
+		archivo.write("<h1>"+elemento["nombre"]+"</h1>"+"\n")
+		archivo.write("<p>"+elemento["horario"]+"</p>"+"\n")
+		archivo.write("<img src="+str(elemento["imagen"])+"/>"+"\n")
+
 
 
